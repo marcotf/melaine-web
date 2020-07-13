9e390c4e36537b8f411b15b2131d21b6be6ccac2
Félix Marcotte-Ruffin <felix.marcotteruffin@gmail.com>
2020-07-13 18:04:09 +0200

    Changing galery plugin

--- components/Galery.vue
+++ components/Galery.vue
@@ -1,28 +1,55 @@
 <template>
   <div class="container mx-auto py-16 px-2">
     <h3>Gallerie</h3>
-    <carousel
-      :perPageCustom="[
-        [1024, 3],
-        [1280, 5]
-      ]"
-      :paginationEnabled="false"
-      :navigationEnabled="true"
-      :scrollPerPage="false"
-      :loop="true"
-      :autoplay="true"
-      navigationNextLabel="→"
-      navigationPrevLabel="←"
-      class="-mx-1"
-    >
-      <slide class="px-1" v-for="i in 23" :key="i">
-        <expandable-image :src="`/images/galery/${i}.jpg`" />
-      </slide>
-    </carousel>
+    <agile :options="agileOptions">
+      <div v-for="i in 23" :key="i" class="px-2">
+        <expandable-image :src="`/images/galery/${i}.jpg`" class="shadow" />
+      </div>
+
+      <!-- <template slot="prevButton">prev</template>
+      <template slot="nextButton">next</template> -->
+    </agile>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      agileOptions: {
+        autoplay: true,
+        speed: 100,
+        navButtons: true,
+
+        responsive: [
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 2
+            }
+          },
+          // {
+          //   breakpoint: 768,
+          //   settings: {
+          //     slidesToShow: 2
+          //   }
+          // },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 1280,
+            settings: {
+              slidesToShow: 5
+            }
+          }
+        ]
+      }
+    }
+  }
+})
 </script>
--- plugins/vue-plugins.js
+++ plugins/vue-plugins.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueCarousel from 'vue-carousel'
 import VueExpandableImage from 'vue-expandable-image'
+import VueAgile from 'vue-agile'
 
 Vue.use(VueCarousel)
 Vue.use(VueExpandableImage)
+Vue.use(VueAgile)
