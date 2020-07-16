d33f2ad1deaf1ed1ac490553c32b5151043a20ac
Félix Marcotte-Ruffin <felix.marcotteruffin@gmail.com>
2020-07-16 17:12:56 +0200

    Adding SEO

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,38 @@
     <nuxt />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      structuredData: {
+        '@context': 'http://www.schema.org',
+        '@type': 'LocalBusiness',
+        name: 'Mélaine Nieuwjaer',
+        image: 'https://melaine-etio-web.herokuapp.com/images/melaine.jpg',
+        telephone: '+33636924394',
+        url: 'https://melaine-etio-web.herokuapp.com',
+        address: {
+          '@type': 'PostalAddress',
+          streetAddress: '874 rue Sainte Catherine',
+          addressLocality: 'LEFFRINCKOUCKE',
+          postalCode: '59495',
+          addressCountry: 'France'
+        },
+        openingHours: 'Mo, Tu, We, Th, Fr 08:00-19:00',
+        contactPoint: {
+          '@type': 'ContactPoint',
+          telephone: '+33636924394'
+        }
+      }
+    }
+  },
+
+  head() {
+    return {
+      script: [{ type: 'application/ld+json', json: this.structuredData }]
+    }
+  }
+}
+</script>
