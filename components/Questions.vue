512b310c52e86237ac8e7e9eda89e8db582704e6
Félix Marcotte-Ruffin <felix.marcotteruffin@gmail.com>
2020-07-16 09:16:11 +0200

    Adding questions list and answer

--- components/Questions.vue
+++ components/Questions.vue
@@ -18,9 +18,11 @@
               <div class="ml-2 font-medium">{{ question.message }}</div>
             </div>
             <transition name="fade">
-              <div v-if="show_answer(question.id)" class="bg-white px-4 pb-4">
-                {{ question.answer }}
-              </div>
+              <div
+                v-if="show_answer(question.id)"
+                class="bg-white px-4 pb-4"
+                v-html="question.answer"
+              />
             </transition>
           </div>
         </div>
@@ -36,9 +38,11 @@
               <div class="ml-2 font-medium">{{ question.message }}</div>
             </div>
             <transition name="fade">
-              <div v-if="show_answer(question.id)" class="bg-white px-4 pb-4">
-                {{ question.answer }}
-              </div>
+              <div
+                v-if="show_answer(question.id)"
+                class="bg-white px-4 pb-4"
+                v-html="question.answer"
+              />
             </transition>
           </div>
         </div>
@@ -58,39 +62,74 @@ export default Vue.extend({
           id: 1,
           message: 'Durée et nombres de séances ?',
           answer:
-            'Les étiopathes reconnues sont recensés dans le Registre national des étiopathes (RNE). L’étiopathie est une thérapie non conventionnée. Comme toute autres alternatives de soins, elle n’est pas remboursée par la sécurité sociale. Aujourd’hui, voyant l’efficacité de l’étiopathie de nombreuses mutuelles remboursent totalement ou en partie les consultations.'
+            '<p>Une séance dure en moyenne 30 min, en fonction de chaque patient. Le nombre de séances dépend de la pathologie et de son ancienneté. Il peut varier entre 2 à 6 séances.</p>'
         },
         {
           id: 2,
-          message: 'Durée et nombres de séances ?',
+          message: 'Quand puis-je consulter un étiopathe ?',
           answer:
-            'Les étiopathes reconnues sont recensés dans le Registre national des étiopathes (RNE). L’étiopathie est une thérapie non conventionnée. Comme toute autres alternatives de soins, elle n’est pas remboursée par la sécurité sociale. Aujourd’hui, voyant l’efficacité de l’étiopathie de nombreuses mutuelles remboursent totalement ou en partie les consultations.'
+            '<p>Vous pouvez consulter un étiopathe lorsqu’une douleur apparaît et devient une gêne importante et récurrente dans votre quotidien. L’étiopathe raisonne uniquement à partir des symptômes du patient. Il appliquera son raisonnement afin d’établir un traitement étiopathique et/ou conduira le patient vers d’autres thérapies si la pathologie ne rentre pas dans son champ d’application.</p>'
         },
         {
           id: 3,
-          message: 'Durée et nombres de séances ?',
+          message: 'Quelles sont les limites de l’étiopathie ?',
           answer:
-            'Les étiopathes reconnues sont recensés dans le Registre national des étiopathes (RNE). L’étiopathie est une thérapie non conventionnée. Comme toute autres alternatives de soins, elle n’est pas remboursée par la sécurité sociale. Aujourd’hui, voyant l’efficacité de l’étiopathie de nombreuses mutuelles remboursent totalement ou en partie les consultations.'
+            '<p>Les limites de la profession concernent les pathologies dites d’atteinte « irréversible», comme les cancers, les maladies dégénératives et génétiques ou les pathologies graves qui relèvent des urgences (maladie infectieuse grave, urgence vitale médicale ou chirurgicale).</p>'
+        },
+        {
+          id: 4,
+          message: 'Champ d’application de l’étiopathie ?',
+          answer:
+            "<p>L'Étiopathie traite les pathologies réversibles manuellement. Elle ne fait pas de prescriptions médicales. Les traitements étiopathiques ne remplacent ni les médicaments lorsqu'ils sont nécessaires, ni la chirurgie lorsqu'elle est indispensable.</p><p>Dans le cas où la pathologie dépasse le champ d’application de l’étiopathie, le praticien adresse son patient vers des soins adaptés (médecin, nutritionniste, psychologue...). L’Étiopathie ne se substitue pas à la médecine traditionnelle, mais se présente comme une solution rapide et efficace. À ce titre, elle reste évidemment complémentaire de la médecine traditionnelle.</p>"
+        },
+        {
+          id: 5,
+          message:
+            'Quelles sont les différences avec d’autres professions de santé et thérapies manuelles ?',
+          answer: `
+              <ul class="list-disc">
+                <li>Le fondement de la pratique étiopathique réside dans sa méthode d’analyse.</li>
+                <li>Chaque étiopathe raisonne à partir de la même méthode analytique et mécaniste et ce dans n’importe quel cabinet en France ou ailleurs. Ainsi, le traitement étiopathique sera le même, peu importe le praticien qui délivre le soin.</li>
+                <li>L’étiopathie considère l’ensemble du corps humain. Elle analyse et met en relation les systèmes de l’organisme. L’étiopathie n’est pas une spécialité médicale.</li>
+                <li>Concernant le traitement étiopathique, il ne s’agit pas de manipuler l’ensemble du corps humain dans sa globalité. Ce n’est pas l’objectif de l’étiopathie. Il s’agit d’être efficace et donc de cibler et traiter uniquement la cause dont découlent les symptômes spécifiques du patient.</li>
+                <li>En étiopathie, la détermination de la cause ne repose pas sur un diagnostic palpatoire. La palpation du praticien est avant tout : subjective. Elle peut néanmoins et parfois conforter une hypothèse pré-établie. Dans tous les cas seul le raisonnement causal fait foi et aboutit à un traitement ciblé et précis.</li>
+                <li>Les techniques manipulatives sont de type mécanique, sans outils, sans médicaments ou autres « pratiques » (énergie, point de pression, lotion, pierre, ...).</li>
+                <li>L’étiopathie s’inscrit dans la promotion de la santé comme alternative de soin naturelle.</li>
+              </ul>
+            `
         }
       ],
       last_questions: [
         {
-          id: 4,
-          message: 'Durée et nombres de séances ?',
-          answer:
-            'Les étiopathes reconnues sont recensés dans le Registre national des étiopathes (RNE). L’étiopathie est une thérapie non conventionnée. Comme toute autres alternatives de soins, elle n’est pas remboursée par la sécurité sociale. Aujourd’hui, voyant l’efficacité de l’étiopathie de nombreuses mutuelles remboursent totalement ou en partie les consultations.'
-        },
-        {
-          id: 5,
-          message: 'Durée et nombres de séances ?',
-          answer:
-            'Les étiopathes reconnues sont recensés dans le Registre national des étiopathes (RNE). L’étiopathie est une thérapie non conventionnée. Comme toute autres alternatives de soins, elle n’est pas remboursée par la sécurité sociale. Aujourd’hui, voyant l’efficacité de l’étiopathie de nombreuses mutuelles remboursent totalement ou en partie les consultations.'
-        },
-        {
           id: 6,
-          message: 'Durée et nombres de séances ?',
+          message: 'Quels sont les bénéfices de l’étiopathie ?',
+          answer: `
+              <ul class="list-disc">
+                <li>Le patient trouvera une solution à ses douleurs, un soulagement, un bien être le plus souvent immédiat même pour des pathologies parfois anciennes.</li>
+                <li>C’est une opportunité pour les entreprises, en réduisant la durée des arrêts de travail, les coûts économiques directs ou indirects (remplacements, indemnité de retard, perte de fabrication...). Les pathologies les plus concernées sont les troubles musculo-squelettiques, les lombalgies par exemple.</li>
+                <li>Elle évite certaines chirurgies ou hospitalisations de dernier recours (hernie discale, canal carpien...), avec souvent un raccourcissement des traitements déjà entrepris (économie de temps et d’argent)</li>
+                <li>En connaissant ses limites, l’étiopathe conseillera au patient des soins pour une prise en charge la plus adaptée à sa pathologie.</li>
+              </ul>
+            `
+        },
+        {
+          id: 7,
+          message:
+            'Faut il consulter son médecin avant de se rendre chez son étiopathe ?',
           answer:
-            'Les étiopathes reconnues sont recensés dans le Registre national des étiopathes (RNE). L’étiopathie est une thérapie non conventionnée. Comme toute autres alternatives de soins, elle n’est pas remboursée par la sécurité sociale. Aujourd’hui, voyant l’efficacité de l’étiopathie de nombreuses mutuelles remboursent totalement ou en partie les consultations.'
+            '<p>Une prescription médicale n’est pas nécessaire pour se rendre chez son étiopathe. Le patient peut donc s’y rendre à sa propre initiative.</p>'
+        },
+        {
+          id: 8,
+          message: 'Tarif et moyen de paiement ?',
+          answer:
+            '<p>Le tarif de la consultation est de 50€ (56 €à domicile) , dont TVA 20 %</p>'
+        },
+        {
+          id: 9,
+          message: 'Reconnaissance et remboursement ?',
+          answer:
+            '<p>Les étiopathes reconnues sont recensés dans le <strong>Registre national des étiopathes (RNE)</strong>. L’étiopathie est une thérapie non conventionnée. Comme toute autres alternatives de soins, elle n’est pas remboursée par la sécurité sociale. Aujourd’hui, voyant l’efficacité de l’étiopathie de nombreuses mutuelles remboursent totalement ou en partie les consultations.</p>'
         }
       ]
     }
