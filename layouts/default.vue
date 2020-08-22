2c6b387b0813ad111138125a03df811206ce8f23
Félix Marcotte-Ruffin <felix.marcotteruffin@gmail.com>
2020-08-22 09:54:49 +0200

    Removing structured data

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,35 +6,34 @@
 
 <script>
 export default {
-  data() {
-    return {
-      structuredData: {
-        '@context': 'http://www.schema.org',
-        '@type': 'LocalBusiness',
-        name: 'Mélaine Nieuwjaer',
-        image: 'https://etiopathedunkerque.fr/images/melaine.jpg',
-        telephone: '+33636924394',
-        url: 'https://etiopathedunkerque.fr/',
-        address: {
-          '@type': 'PostalAddress',
-          streetAddress: '874 rue Sainte Catherine',
-          addressLocality: 'LEFFRINCKOUCKE',
-          postalCode: '59495',
-          addressCountry: 'France'
-        },
-        openingHours: 'Mo, Tu, We, Th, Fr 08:30-19:30 Sa 08:30-12:30',
-        contactPoint: {
-          '@type': 'ContactPoint',
-          telephone: '+33636924394'
-        }
-      }
-    };
-  },
-
-  head() {
-    return {
-      script: [{ type: 'application/ld+json', json: this.structuredData }]
-    };
-  }
+  // data() {
+  //   return {
+  //     structuredData: {
+  //       '@context': 'http://www.schema.org',
+  //       '@type': 'LocalBusiness',
+  //       name: 'Mélaine Nieuwjaer',
+  //       image: 'https://etiopathedunkerque.fr/images/melaine.jpg',
+  //       telephone: '+33636924394',
+  //       url: 'https://etiopathedunkerque.fr/',
+  //       address: {
+  //         '@type': 'PostalAddress',
+  //         streetAddress: '874 rue Sainte Catherine',
+  //         addressLocality: 'LEFFRINCKOUCKE',
+  //         postalCode: '59495',
+  //         addressCountry: 'France'
+  //       },
+  //       openingHours: 'Mo, Tu, We, Th, Fr 08:00-19:30 Sa 08:00-12:30',
+  //       contactPoint: {
+  //         '@type': 'ContactPoint',
+  //         telephone: '+33636924394'
+  //       }
+  //     }
+  //   };
+  // },
+  // head() {
+  //   return {
+  //     script: [{ type: 'application/ld+json', json: this.structuredData }]
+  //   };
+  // }
 };
 </script>
